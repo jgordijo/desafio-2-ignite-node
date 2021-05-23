263f97a60415d70fbf8b63fa75b7bfff9e5ae1a2
Julio <julio.filho@milvus.com.br>
2021-05-23 15:37:12 -0300

    all tests passed 🚀

--- src/index.js
+++ src/index.js
@@ -10,19 +10,66 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers,
+    user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "Invalid username!" });
+  }
+
+  request.user = user;
+
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && user.todos.length === 10) {
+    return response.status(403).json({ error: 'Todos limit reached!' });
+  }
+
+  next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers,
+    { id } = request.params,
+    user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "Invalid username!" });
+  }
+
+  const validId = validate(id);
+
+  if (!validId) {
+    return response.status(400).json({ error: "Invalid todo ID!" });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found!" });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params,
+    user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found!" });
+  }
+
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
